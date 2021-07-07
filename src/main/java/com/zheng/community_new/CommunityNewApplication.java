ea00d5fdcf75e5bc8e082ee2034881b246caf092
GaraltKnows <2654881933@qq.com>
2021-07-07 13:05:31 +0800

    init repo

---
+++ src/main/java/com/zheng/community_new/CommunityNewApplication.java
@@ -0,0 +1,13 @@
+package com.zheng.community_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zheng/community_new/CommunityNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zheng.community_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
